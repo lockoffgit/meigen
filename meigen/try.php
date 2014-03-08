aa94e65383db1f63176eec996d4de19d08637e86
genzouw <genzouw@gmail.com>
2014-03-08 15:11:18 +0900

    仮デザイン（bootstrap）

--- meigen/try.php
+++ meigen/try.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<title>だいたいわかりましたページ - 名言くん</title>
+	</head>
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<title>詳細</title>
-</head>
+	<body>
+		<div class="navbar navbar-default">
+			<div class="navbar-inner">
+				<div class="container">
+					<h1><a href="javascript:void(0);" class="brand">名言くん</a></h1>
+					<ul class="nav pull-right">
+						<li><a href="javascript:void(0);">ここにーー</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
-<body>
-	<a href="../meigen/meigen.php">名言</a>
-	<a href="../meigen/ymt_nyuu.php">使ってみた登録</a>
-</body>
+		<div class="container">
+			<h2>だいたいわかりましたページ</h2>
+
+			<div>
+				<a href="../meigen/meigen.php">名言</a>
+				<a href="../meigen/ymt_nyuu.php">使ってみた登録</a>
+			</div>
+
+		</div>
+
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+	</body>
+</html>
+
 
 </html>
