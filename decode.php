6f559eba592e4489303072bb18e1e875c9402877
massan77 <masahiro@mbp.local>
2014-03-08 15:36:14 +0900

    認証操作を追加

---
+++ decode.php
@@ -0,0 +1,303 @@
+<?php
+
+$json = <<<EOB
+{
+  "data": [
+    {
+      "id": "100007358252557", 
+      "first_name": "修平", 
+      "last_name": "高橋"
+    }, 
+    {
+      "id": "100003843073831", 
+      "first_name": "慎一", 
+      "last_name": "高橋"
+    }, 
+    {
+      "id": "100004032172283", 
+      "first_name": "雅博", 
+      "last_name": "松本"
+    }, 
+    {
+      "id": "100006637784375", 
+      "first_name": "Saori", 
+      "last_name": "Yamamoto"
+    }, 
+    {
+      "id": "100002046015199", 
+      "first_name": "聡", 
+      "last_name": "小原"
+    }, 
+    {
+      "id": "100002000747491", 
+      "first_name": "悠", 
+      "last_name": "澤井"
+    }, 
+    {
+      "id": "100000906116393", 
+      "first_name": "綾子", 
+      "last_name": "横山"
+    }, 
+    {
+      "id": "100002206323763", 
+      "first_name": "淳", 
+      "last_name": "宮川"
+    }, 
+    {
+      "id": "100002076594044", 
+      "first_name": "健太郎", 
+      "last_name": "松本"
+    }, 
+    {
+      "id": "100003481638265", 
+      "first_name": "崇", 
+      "last_name": "小岡"
+    }, 
+    {
+      "id": "100002932791064", 
+      "first_name": "裕美", 
+      "last_name": "村上"
+    }, 
+    {
+      "id": "100002313420706", 
+      "first_name": "聡志", 
+      "last_name": "水野"
+    }, 
+    {
+      "id": "100002192337366", 
+      "first_name": "愛樹", 
+      "last_name": "足立"
+    }, 
+    {
+      "id": "100001996069879", 
+      "first_name": "篤紀", 
+      "last_name": "満田"
+    }, 
+    {
+      "id": "100001834624941", 
+      "first_name": "直樹", 
+      "last_name": "梶原"
+    }, 
+    {
+      "id": "100001775661591", 
+      "first_name": "美穂", 
+      "last_name": "加嶋"
+    }, 
+    {
+      "id": "100001649308458", 
+      "first_name": "仁", 
+      "last_name": "中川"
+    }, 
+    {
+      "id": "100001556212644", 
+      "first_name": "Bunta", 
+      "last_name": "Mabuchi"
+    }, 
+    {
+      "id": "100001400368922", 
+      "first_name": "優希", 
+      "last_name": "纐纈"
+    }, 
+    {
+      "id": "100001170900189", 
+      "first_name": "陽信", 
+      "last_name": "金"
+    }, 
+    {
+      "id": "100001165486449", 
+      "first_name": "紗弥佳", 
+      "last_name": "池田"
+    }, 
+    {
+      "id": "100001025464798", 
+      "first_name": "正拡", 
+      "last_name": "小林"
+    }, 
+    {
+      "id": "1636810292", 
+      "first_name": "Mitsuaki", 
+      "last_name": "Senda"
+    }, 
+    {
+      "id": "1263139141", 
+      "first_name": "Ag", 
+      "last_name": "Toyosawa"
+    }, 
+    {
+      "id": "11713944", 
+      "first_name": "Minami", 
+      "last_name": "Nakayama"
+    }, 
+    {
+      "id": "100002309593818", 
+      "first_name": "貴信", 
+      "last_name": "竹上"
+    }, 
+    {
+      "id": "100002242698019", 
+      "first_name": "ちか", 
+      "last_name": "かとう"
+    }, 
+    {
+      "id": "100002085036662", 
+      "first_name": "晋平", 
+      "last_name": "畑"
+    }, 
+    {
+      "id": "100002061307208", 
+      "first_name": "浩明", 
+      "last_name": "銭谷"
+    }, 
+    {
+      "id": "100001915898206", 
+      "first_name": "渉", 
+      "last_name": "河手"
+    }, 
+    {
+      "id": "100001914425531", 
+      "first_name": "賢也", 
+      "last_name": "上原"
+    }, 
+    {
+      "id": "100001790874135", 
+      "first_name": "圭太郎", 
+      "last_name": "岸田"
+    }, 
+    {
+      "id": "100001780523053", 
+      "first_name": "奈緒子", 
+      "last_name": "徳島"
+    }, 
+    {
+      "id": "100001779440281", 
+      "first_name": "Kayoko", 
+      "last_name": "Nagano"
+    }, 
+    {
+      "id": "100001705794752", 
+      "first_name": "肇", 
+      "last_name": "内田"
+    }, 
+    {
+      "id": "100001691813229", 
+      "first_name": "真衣", 
+      "last_name": "圓増"
+    }, 
+    {
+      "id": "100001601606214", 
+      "first_name": "俊介", 
+      "last_name": "三原"
+    }, 
+    {
+      "id": "100001004509971", 
+      "first_name": "健太郎", 
+      "last_name": "大河内"
+    }, 
+    {
+      "id": "1013296321", 
+      "first_name": "博一", 
+      "last_name": "福田"
+    }, 
+    {
+      "id": "100003657657905", 
+      "first_name": "Kawamura", 
+      "last_name": "Tsutomu"
+    }, 
+    {
+      "id": "100002483620307", 
+      "first_name": "謙介", 
+      "last_name": "赤木"
+    }, 
+    {
+      "id": "100002453603179", 
+      "first_name": "泰弘", 
+      "last_name": "笹部"
+    }, 
+    {
+      "id": "100002048531141", 
+      "first_name": "弘子", 
+      "last_name": "範国"
+    }, 
+    {
+      "id": "100001960940194", 
+      "first_name": "啓顕", 
+      "last_name": "吉本"
+    }, 
+    {
+      "id": "100001803382925", 
+      "first_name": "淳司", 
+      "last_name": "梅本"
+    }, 
+    {
+      "id": "100001750307868", 
+      "first_name": "加奈子", 
+      "last_name": "又座"
+    }, 
+    {
+      "id": "100001193630184", 
+      "first_name": "稔人", 
+      "last_name": "一木"
+    }, 
+    {
+      "id": "100000780472939", 
+      "first_name": "進", 
+      "last_name": "岩田"
+    }, 
+    {
+      "id": "607201812", 
+      "first_name": "Miho", 
+      "last_name": "Maeyama"
+    }, 
+    {
+      "id": "100002213434335", 
+      "first_name": "智広", 
+      "last_name": "足立"
+    }, 
+    {
+      "id": "100001601821593", 
+      "first_name": "佳奈", 
+      "last_name": "内田"
+    }, 
+    {
+      "id": "595924652", 
+      "first_name": "隆幸", 
+      "last_name": "中西"
+    }, 
+    {
+      "id": "100001840053970", 
+      "first_name": "啓志", 
+      "last_name": "垣中"
+    }, 
+    {
+      "id": "100001803589020", 
+      "first_name": "Akiko", 
+      "last_name": "Kiriu"
+    }, 
+    {
+      "id": "100003288612600", 
+      "first_name": "健司", 
+      "last_name": "青柳"
+    }, 
+    {
+      "id": "100002304143011", 
+      "first_name": "計蔵", 
+      "last_name": "宇野"
+    }, 
+    {
+      "id": "100001872483422", 
+      "first_name": "香保里", 
+      "last_name": "小川"
+    }, 
+    {
+      "id": "534399468", 
+      "first_name": "岳志", 
+      "last_name": "西野"
+    }
+  ]
+}
+EOB;
+$data = json_decode($json, true);
+
+foreach ($data['data'] as $member) {
+	echo $member['id'] . "\t" . $member['last_name'] . "\t" . $member['first_name'] . "\n";
+}
