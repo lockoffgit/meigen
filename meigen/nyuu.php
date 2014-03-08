17afafdae9c8af5e0f7bcf98cd4b21fc6917cfe2
Akari Fujimoto <oe7g.ax@gmail.com>
2014-03-08 19:26:16 +0900

    temp commit

--- meigen/nyuu.php
+++ meigen/nyuu.php
@@ -1,13 +1,121 @@
+<?php
+
+require('../libs/database.php');
+
+    // $objDb->insert("items", array("id" => "321", "price" => "1234"));
+    // $objDb->select("SELECT * FROM items WHERE id > ?", array("1"));
+
+$objDb = new db_util();
+if (count($_POST) != 0) {
+var_dump($_POST);
+	$contributor = $_SESSION['facebook_user_id'];
+
+	$member_id = htmlspecialchars($_POST['speaker'], ENT_QUOTES);
+	$person = $objDb->select("SELECT name FROM members WHERE member_id=$member_id;");
+	foreach($person as $val){
+    	$speaker = $val['name'];
+	}
+
+	$meigen_text = htmlspecialchars($_POST['meigen_text'], ENT_QUOTES);
+	$unit_name = htmlspecialchars($_POST['unit_name'], ENT_QUOTES);
+	$situation = htmlspecialchars($_POST['situation'], ENT_QUOTES);
+	$font = htmlspecialchars($_POST['font'], ENT_QUOTES);
+	$image_url = '';
+	$addParams = array(
+		"contributor"	=> $contributor,
+	//	"speaker"		=> $speaker,
+		"meigen_text"	=> $meigen_text,
+	//	"unit_name"		=> $unit_name,
+	//	"situation"		=> $situation,
+	//	"font"			=> $font,
+	//	"member_id"		=> $member_id,
+	//	"image_url"		=> $image_url
+	);
+
+	var_dump($objDb->insert("meigens", $addParams));
+
+	// header("Location: ./meigen.php");
+	exit();
+}
+
+
+// if (isset($_POST)) {
+
+// 	$meigen_id = 'SELECT COUNT(*) FROM meigens'
+	
+// 	$query = 'SELECT COUNT(*) FROM meigens';
+// 	$rnum = mysql_query($query);
+// 	list($meigen_id) = mysql_fetch_row($rnum);
+
+
+
+
+	// print('<p>' . $meigen_text . '</p>');
+	// print('<p>' . $situation . '</p>');
+	// print('<p>' . $font . '</p>');
+
+// 	$sql = 'INSERT INTO meigens (meigen_id, contributor, speaker, meigen_text, unit_name, situation, font, member_id, image_url) VALUES ($meigen_id, $contributor, $speaker, $meigen_text, $unit_name, $situation, $font, $member_id, $image_url)';
+// 	$result_flag = mysql_query($sql);
+
+// }
+
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<title>にゅー</title>
+	<title>名言新規登録</title>
 </head>
 
 <body>
-	<a href="../meigen/meigen.php">めいげん</a>
+
+	<form name="form_regist" action="./nyuu.php" method="POST">
+		<p>発言者(必須): 
+			<select name="speaker" id="speaker">
+				<?php
+
+    			$member = $objDb->select("SELECT member_id, name FROM members;");
+    		 	foreach($member as $val){
+    				print('<option value="' . $val['member_id'] . '">' . $val['name'] . '</option>');
+				}
+
+				?>
+			</select>
+		</p>
+
+		<p>ユニット名: 
+			<select name="unit_name">
+				<?php
+				
+    			$unit = $objDb->select("SELECT unit_name FROM units;");
+    		 	foreach($unit as $val){
+    				print('<option value="' . $val['unit_name'] . '">' . $val['unit_name'] . '</option>');
+				}
+
+				?>
+
+			</select>
+
+		<!-- <p>写真の選択: <input type="file" name="img" id="img" /></p> -->
+
+		<p>名言(必須): <input type="text" name="meigen_text" id="meigen_text" maxlength="255" /></p>
+
+		<p>フォント(必須): </p>
+		<p>
+			<input type="radio" name="font" id="font" value="font1" /><label for="font">フォント1</label>
+			<input type="radio" name="font" id="font" value="font2" /><label for="font">フォント2</label>
+			<input type="radio" name="font" id="font" value="font3" /><label for="font">フォント3</label>
+		</p>
+
+		<p>名言の背景(必須): </p>
+		<p><textarea name="situation" id="situation" rows="5" column="20" maxlength="255"></textarea></p>
+
+		<input type="submit" value="投稿する" />
+	</form>
+
+	<br>
+	<a href="../meigen/meigen.php">名言</a>
 </body>
 
 </html>
