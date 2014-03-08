d0a59ddc3481fdfea50b8704f3e35ceecbf818bb
Akari Fujimoto <oe7g.ax@gmail.com>
2014-03-08 11:19:52 +0900

    紙芝居

---
+++ meigen/ymt_risuto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>やってみた</title>
+</head>
+
+<body>
+	<a href="../meigen/meigen.php">めいげん</a>
+	<a href="../meigen/ymt_nyuu.php">やってみた投稿</a>
+</body>
+
+</html>
