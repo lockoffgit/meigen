9af816b48ed33b36e49d9bc6042f072fd39a5254
Yangsin <poeyangstar@gmail.com>
2014-03-08 17:18:29 +0900

    ぼちをとりあえず作成
    デザインがわからないので、テーブルで許して下さいませ。

--- bochi/index.php
+++ bochi/index.php
@@ -1,7 +1,8 @@
 <?php
 require('../libs/database.php');
+$objDb = new db_util();
+$graves = $objDb->select("SELECT * FROM graves");
 
-$graves = ["grave_id" => "1", "meigen_text" => "大は小をかねない", "speaker" => "富田", "contributor" => '上原', 'killer' => 'みはら', 'together' => '3']; 
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +13,42 @@ $graves = ["grave_id" => "1", "meigen_text" => "大は小をかねない", "spea
 </head>
 
 <body>
+
+<div id="graves-list">
+<table>
+    <tr>
+        <td>名言</td>
+        <td>発言者</td>
+        <td>投稿者</td>
+        <td>犯人</td>
+        <td>犯行時刻</td>
+        <td>道連れ数</td>
+    </tr>
+    <?php
+    if (count($graves) > 0) {
+        foreach($graves as $grave) {
+    ?>
+        <tr>
+            <td><?php echo $grave['meigen_text'] ?></td>
+            <td><?php echo $grave['speaker'] ?></td>
+            <td><?php echo $grave['contributor'] ?></td>
+            <td><?php echo $grave['killer'] ?></td>
+            <td><?php echo $grave['created_at'] ?></td>
+            <td><?php echo $grave['together'] ?></td>
+        </tr>
+    <?php
+
+        }
+    } else {
+    ?>
+        <td colspan="6">墓は空のようだ</td>
+    <?php
+    }
+    ?>
+</table>
+</div>
 	<a href="../meigen/meigen.php">めいげん</a>
 </body>
 
 </html>
+
