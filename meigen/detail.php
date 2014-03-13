c50efcb16d100e9d3cc6c9f8abb07b82df4f988e
genzouw <genzouw@gmail.com>
2014-03-13 22:39:29 +0900

    詳細ページのデザインを気持ちあわせた

--- meigen/detail.php
+++ meigen/detail.php
@@ -13,68 +13,80 @@ if (is_null($meigen)) {
 
 $tries = $objDb->select('select * from tries where meigen_id = ? order by try_id desc', [ $params['meigen_id'] ]);
 ?><!DOCTYPE html>
-<html lang="ja">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<title>だいたいわかりましたページ - 名言くん</title>
-	</head>
+<html class="no_js" lang="ja">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="description" content="">
+	<link type="text/css" rel="stylesheet" href="../css/main.css" />
+	<title>名言くん（仮）</title>
+</head>
 
-	<body>
-		<div class="navbar navbar-default">
-			<div class="navbar-inner">
-				<div class="container">
-					<h1><a href="javascript:void(0);" class="brand">名言くん</a></h1>
-					<ul class="nav pull-right">
-						<li><a href="javascript:void(0);">ここにアカウント名</a></li>
-					</ul>
+<body>
+<div class="container">
+	<div class="navbar">
+		<div class="navbar-header">
+			<div class="navbar-brand">
+				<a href="index.html">名言くん</a>
+			</div>
+			<div class="navbar-entry"><a href="./nyuu.php">投稿する</a></div>
+		</div>
+	</div>
+
+	<div class="navbar-collapse collapse">
+		<ul class="navbar-nav">
+			<li><a href="work.html">RANKING</a></li>
+			<li><a href="about.html">CALLENDER</a></li>
+			<li><a href="blog.html">SHINIKAKE</a></li>
+			<li><a href="contact.html">BOCHI</a></li>
+		</ul>
+	</div>
+
+
+	<div class="meigen">
+		<div class="meigen-area">
+			<div class="meigen-photo"><img src="<?php echo $meigens['meigen_image_url']; ?>"></div>
+			<div class="meigen-txt">
+				<div class="meigen-detail">
+				<h2>高橋くんデザインお願いします</h2>
+				<?php
+					echo '<p>' . nl2br(htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['contributor'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['speaker'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['meigen_text'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['unit_name'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['situation'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['font'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['member_id'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['image_url'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['created_at'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($meigen['modified_at'], ENT_QUOTES,'UTF-8')) . '</p>';
+				?>
+				</div>
+				<div class="meigen-detail">
+				<?php
+				foreach ($tries as $try) {
+					echo '<p>' . nl2br(htmlspecialchars($try['contributor'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['meigen_id'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['unit_name'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['font'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['member_id'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['image_url'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['created_at'], ENT_QUOTES,'UTF-8')) . '</p>';
+					echo '<p>' . nl2br(htmlspecialchars($try['modified_at'], ENT_QUOTES,'UTF-8')) . '</p>';
+				}
+				?>
 				</div>
 			</div>
 		</div>
-
-		<div class="container">
-			<h2>だいたいわかりましたページ</h2>
-
-			<div>
-				<h3>詳細</h3>
-<?php
-echo '<p>' . nl2br(htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['contributor'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['speaker'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['meigen_text'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['unit_name'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['situation'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['font'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['member_id'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['image_url'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['created_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-echo '<p>' . nl2br(htmlspecialchars($meigen['modified_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-?>
-
-				<h3>だいたいわかりました</h3>
-<?php
-foreach ($tries as $try) {
-	echo '<p>' . nl2br(htmlspecialchars($try['contributor'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['meigen_id'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['unit_name'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['font'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['member_id'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['image_url'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['created_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-	echo '<p>' . nl2br(htmlspecialchars($try['modified_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-}
-?>
-
-				<a href="../meigen/index.php">名言</a>
-				<a href="../meigen/try.php?meigen_id=<?php echo htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>">使ってみた登録</a>
-			</div>
+		<div class="ha-area">
+			<ul>
+				<li><a href="../meigen/index.php">名言</a></li>
+				<li><a href="../meigen/try.php?meigen_id=<?php echo htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>">使ってみた登録</a></li>
+			</ul>
 		</div>
+	</div>
 
-		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
-	</body>
+</div>
+<a href="../meigen/">めいげん</a>
+</body>
 </html>
