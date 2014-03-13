8b6bfca59c0bad8715fb8ae1c0e34f1e888feaaf
massan77 <masahiro@mbp.local>
2014-03-13 22:40:51 +0900

    認証機能をコミット。設定はfacebookにて

--- auth/login_filter.php
+++ auth/login_filter.php
@@ -11,12 +11,14 @@ $log->pushHandler(new StreamHandler('/var/log/meigen/app.log', Logger::DEBUG));
  * 未ログインのユーザをログインページへ誘導するフィルタ
  */
 $originalURI = $_SERVER['REQUEST_URI'];
+$isSkipAuth = false;
+/*
 $log->addInfo('login_filter REQUEST_URI = ' . $originalURI);
 $isSkipAuth =
 	   (preg_match('|^/facebook.*$|', $originalURI))
 	|| (preg_match('|^/auth.*$|', $originalURI))
 	|| (preg_match('|^/callback.*$|', $originalURI));
-
+*/
 $log->addDebug('login_token = ' . $_COOKIE['login_token']);
 
 if ($isSkipAuth) {
