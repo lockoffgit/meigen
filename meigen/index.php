3ce8a8132578c45adc930c0b648d3bf4188bb8c5
genzouw <genzouw@gmail.com>
2014-03-13 21:04:00 +0900

    SQLがおかしかったので修正

--- meigen/index.php
+++ meigen/index.php
@@ -1,4 +1,33 @@
-<!DOCTYPE html>
+<?php
+    require_once("../libs/database.php");
+    define("LIST_MAX", 10);
+    $objDb = new db_util();
+    $query = "
+    SELECT
+        Mei.*
+        ,COUNT(Iot.iotw_id) AS iotw_cnt
+        ,COUNT(del.delete_id) AS delete_cnt
+        ,COALESCE(
+            Mei.image_url
+            ,Mem.image_url
+        ) AS meigen_image_url
+    FROM
+        meigens AS Mei LEFT OUTER JOIN members AS Mem
+            ON Mem.member_id = Mei.member_id LEFT OUTER JOIN iotws AS Iot
+            ON Iot.meigen_id = Mei.meigen_id LEFT OUTER JOIN deletes AS del
+            ON del.meigen_id = Mei.meigen_id
+    WHERE
+        Mem.del_flag <> 1
+    GROUP BY
+        Iot.meigen_id
+        ,del.meigen_id
+    ORDER BY
+        modified_at DESC
+    ";
+    $page_no = $_GET['page_no'] ? ($_GET['page_no'] - 1) * LIST_MAX : 0;
+    $params[] = $page_no;
+    $arrMeigens = $objDb->select($query, $params);
+?><!DOCTYPE html>
 <html class="no_js" lang="ja">
 <head>
 <meta charset="UTF-8" />
@@ -29,38 +58,6 @@
   </ul>
 </div>
 
-<?php
-    require_once("../libs/database.php");
-    define("LIST_MAX", 10);
-    $objDb = new db_util();
-    $query = "
-        SELECT 
-            Mei.*, COUNT(Iot.iotw_id) AS iotw_cnt, COUNT(Del.delete_id) AS delete_cnt
-            COALESCE(Mei.image_url, Mem.image_url) AS meigen_image_url
-
-        FROM meigens AS Mei
-        
-        LEFT JOIN members AS Mem
-        ON Mem.member_id = Mei.member_id
-        
-        LEFT JOIN iotws AS Iot
-        ON Iot.meigen_id = Mei.meigen_id
-        
-        LEFT JOIN deletes AS Del
-        ON Del.meigen_id = Mei.meigen_id
-        
-        WHERE Mem.del_flag <> 1
-
-        GROUP BY Iot.meigen_id , Del.meigen_id
-        
-        ORDER BY updated_at DESC
-
-        LIMIT ?, ".LIST_MAX."
-    ";
-    $page_no = $_GET['page_no'] ? ($_GET['page_no'] - 1) * LIST_MAX : 0;
-    $params[] = $page_no;
-    $arrMeigens = $objDb->select($query, $params);
-?>
 
 <!--名言ここから-->
 <?php
