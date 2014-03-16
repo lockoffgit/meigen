4779703b6b07147c2ac54914cea7f43071a05747
ta-ra-ra <adachi@192.168.11.9>
2014-03-17 00:27:06 +0900

    まっさんの環境構築にあわせる

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "install packages"
+# install packages
 sudo yum install httpd -y
-sudo yum install mysql-devel mysql-server -y
+sudo yum install mysql-server -y
 sudo yum install php php-mysql php-pdo -y
 
-echo "setup mysqld"
+# setup mysqld
 sudo /etc/init.d/mysqld start
 sudo chkconfig mysqld on
 mysql -uroot < /vagrant_data/ddl/01_create_database.sql 
@@ -13,8 +13,11 @@ mysql -uroot -Dmeigen_db < /vagrant_data/ddl/02_create_table.sql
 mysql -uroot -Dmeigen_db < /vagrant_data/ddl/03_foreign_key.sql 
 mysql -uroot -Dmeigen_db < /vagrant_data/ddl/04_index.sql 
 
-echo "setup httpd"
+# setup httpd
+sudo mkdir /var/log/meigen
+sudo chmod 777 /var/log/meigen
 sudo rm -rf /var/www/html
 sudo ln -s /vagrant_data /var/www/html
+sudo ln -s /vagrant_data /var/www/meigenkun
 sudo /etc/init.d/httpd start
 sudo chkconfig httpd on
