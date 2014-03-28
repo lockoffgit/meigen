c50a232bc224bf2ab43dbc55cfb944e27e956f4d
genzouw <genzouw@gmail.com>
2014-03-28 22:34:16 +0900

    雰囲気わかりましたページ実装完了

--- meigen/try.php
+++ meigen/try.php
@@ -1,41 +1,90 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php
+require_once('../libs/core.php');
+
+session_start();
+
+$params = array_merge($_POST, $_GET);
+
+$objDb = new db_util();
+
+$meigen = array_pop($objDb->select('select * from meigens where meigen_id = ?', [ $params['meigen_id'] ]));
+if (is_null($meigen)) {
+	header("HTTP/1.0 404 Not Found");
+	die();
+}
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+	$member_id = $params['member_id'];
+	$member = array_pop($objDb->select('select * from members where member_id = ?', [ $member_id ]));
+	if (is_null($member)) {
+		header("HTTP/1.0 404 Not Found");
+		die();
+	}
+
+	// TODO デバッグ
+	// $contributor = $_SESSION['facebook_user_id'];
+	$contributor = 'dummy-contributorr';
+
+	$unit = array_pop($objDb->select('select * from units where unit_id = ?', [ $member['unit_id'] ]));
+	if (is_null($unit)) {
+		header("HTTP/1.0 404 Not Found");
+		die();
+	}
+
+	$font = $params['font'];
+	$image_url = '';
+
+	$modified_at = date("Y-m-d H:i:s");
+	$addParams = array(
+		'meigen_id' => ''.$meigen['meigen_id'],
+		"contributor" => $contributor,
+		"unit_name" => $unit['unit_name'],
+		"font" => $font,
+		"member_id" => ''.$member['member_id'],
+		"image_url" => $image_url,
+		"created_at" => $modified_at,
+		"modified_at" => $modified_at,
+	);
+
+	$result = $objDb->insert("tries", $addParams);
+
+	header("Location: ./detail.php?meigen_id=".$meigen['meigen_id']);
+	exit();
+}
+
+$members = $objDb->select("SELECT * FROM members;");
+?><!DOCTYPE html>
+<html>
 	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-		<meta name="keywords" content="" />
-		<meta name="description" content="" />
-		<title>だいたいわかりましたページ - 名言くん</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<title>雰囲気わかりました</title>
 	</head>
-
 	<body>
-		<div class="navbar navbar-default">
-			<div class="navbar-inner">
-				<div class="container">
-					<h1><a href="javascript:void(0);" class="brand">名言くん</a></h1>
-					<ul class="nav pull-right">
-						<li><a href="javascript:void(0);">ここにーー</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+		<form name="form_regist" action="./try.php" method="POST">
+			<input type="hidden" name="meigen_id" value="<?php print htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>" id="meigen_id" />
+			<p>発言者(必須): 
+				<select name="member_id" id="member_id">
+					<?php
+					foreach($members as $val){
+						print('<option value="' . $val['member_id'] . '">' . $val['name'] . '</option>');
+					}
+					?>
+				</select>
+			</p>
 
-		<div class="container">
-			<h2>だいたいわかりましたページ</h2>
+			<!-- <p>写真の選択: <input type="file" name="img" id="img" /></p> -->
 
-			<div>
-				<a href="../meigen/meigen.php">名言</a>
-				<a href="../meigen/ymt_nyuu.php">使ってみた登録</a>
-			</div>
+			<p>フォント(必須): </p>
+			<p>
+				<input type="radio" name="font" id="font" value="font1" /><label for="font">フォント1</label>
+				<input type="radio" name="font" id="font" value="font2" /><label for="font">フォント2</label>
+				<input type="radio" name="font" id="font" value="font3" /><label for="font">フォント3</label>
+			</p>
 
-		</div>
+			<input type="submit" value="投稿する" />
+		</form>
 
-		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+		<br>
+		<a href="../meigen/detail.php?meigen_id=<?php print htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>">名言</a>
 	</body>
-</html>
-
-
 </html>
