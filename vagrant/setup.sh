d22cd2f13bdf8fce95ac9c67322523a77899f02b
ta-ra-ra <chihiro.adachi@gmail.com>
2014-03-28 21:04:24 +0900

    うごくように修正

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -3,7 +3,7 @@
 # install packages
 sudo yum install httpd -y
 sudo yum install mysql-server -y
-sudo yum install php php-mysql php-pdo -y
+sudo yum install php php-mysql php-pdo php-mbstring -y
 
 # setup mysqld
 sudo /etc/init.d/mysqld start
@@ -16,9 +16,8 @@ mysql -uroot -Dmeigen_db < /vagrant_data/ddl/04_index.sql
 # setup httpd
 sudo mkdir /var/log/meigen
 sudo chmod 777 /var/log/meigen
-sudo rm -rf /var/www/html
-sudo ln -s /vagrant_data /var/www/html
 # .htaccessがmeigenkunをみてたのでシンボリックリンク追加
 sudo ln -s /vagrant_data /var/www/meigenkun
+sudo cp -f /vagrant/httpd.conf /etc/httpd/conf/httpd.conf
 sudo /etc/init.d/httpd start
 sudo chkconfig httpd on
