71b8cd1645d23c830e6c0d45dbcb70dc03aa8b9c
massan77 <masahiro@mbp.local>
2014-04-03 12:29:12 +0900

    認証機能を追加

--- auth/login_filter.php
+++ auth/login_filter.php
@@ -11,21 +11,14 @@ $log->pushHandler(new StreamHandler('/var/log/meigen/app.log', Logger::DEBUG));
  * 未ログインのユーザをログインページへ誘導するフィルタ
  */
 $originalURI = $_SERVER['REQUEST_URI'];
-$isSkipAuth = false;
-/*
 $log->addInfo('login_filter REQUEST_URI = ' . $originalURI);
 $isSkipAuth =
-	   (preg_match('|^/facebook.*$|', $originalURI))
+	   (preg_match('|^/meigen/facebook.*$|', $originalURI))
 	|| (preg_match('|^/auth.*$|', $originalURI))
 	|| (preg_match('|^/callback.*$|', $originalURI));
-*/
+
 $log->addDebug('login_token = ' . $_COOKIE['login_token']);
 
-if ($isSkipAuth) {
-	$log->addDebug("isAuthPage is true.");
-} else {
-	$log->addDebug("isAuthPage is false.");
-}
 $log->addDebug('$isSkipAuth = ' . $isSkipAuth);
 
 if (!$isSkipAuth && !isset($_COOKIE['login_token'])) {
@@ -36,4 +29,6 @@ if (!$isSkipAuth && !isset($_COOKIE['login_token'])) {
 
 	header("Location: /facebook",true,303);
 }
+
+
 
