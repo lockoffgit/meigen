8be5f860ced515f75e2e2baa74f1f5f0162a3339
kuehara <parasan.mail@gmail.com>
2014-04-04 13:29:25 +0900

    ID取得部分をコミット

--- meigen/detail.php
+++ meigen/detail.php
@@ -1,92 +1,77 @@
 <?php
 require_once('../libs/core.php');
 
-$params = array_merge($_POST, $_GET);
+$params = $_GET;
 
 $objDb = new db_util();
 
-$meigen = array_pop($objDb->select('select * from meigens where meigen_id = ?', [ $params['meigen_id'] ]));
+$meigen = array_pop($objDb->select('select * from meigens where meigen_id = ?', array($params['meigen_id'])));
 if (is_null($meigen)) {
 	header("HTTP/1.0 404 Not Found");
 	die();
 }
 
-$tries = $objDb->select('select * from tries where meigen_id = ? order by try_id desc', [ $params['meigen_id'] ]);
+$tries = $objDb->select('select * from tries where meigen_id = ? order by try_id desc', array($params['meigen_id']));
+
 ?><!DOCTYPE html>
 <html class="no_js" lang="ja">
 <head>
-	<meta charset="UTF-8" />
-	<meta name="description" content="">
-	<link type="text/css" rel="stylesheet" href="../css/main.css" />
-	<title>名言くん（仮）</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>名言くん（仮） ver.amagasaki - 詳細 -</title>
+    <meta name="description" content="">
+    <link type="text/css" rel="stylesheet" href="../css/main.css" />
 </head>
 
-<body>
 <div class="container">
-	<div class="navbar">
-		<div class="navbar-header">
-			<div class="navbar-brand">
-				<a href="./">名言くん</a>
-			</div>
-			<div class="navbar-entry"><a href="./nyuu.php">投稿する</a></div>
-		</div>
-	</div>
-
-	<div class="navbar-collapse collapse">
-		<ul class="navbar-nav">
-			<li><a href="work.html">RANKING</a></li>
-			<li><a href="about.html">CALLENDER</a></li>
-			<li><a href="blog.html">SHINIKAKE</a></li>
-			<li><a href="contact.html">BOCHI</a></li>
+	<header class="cf">
+            <div class="navbar-brand"> <a href="./"><img src="../img/title.png"></a></div>
+		<div class="navbar-entry"><a href="./nyuu.php"><img src="../img/bt_entry.png" alt="投稿する"></a></div>
+	</header>
+	<nav>
+		<ul class="navbar-nav cf">
+			<li><a href="javascript:alert('かみんぐすぅん');"><img src="../img/nav_ranking.png" alt="ランキング"></a></li>
+			<li><a href="javascript:alert('かみんぐすぅん');"><img src="../img/nav_cal.png" alt="カレンダー"></a></li>
+			<li><a href="javascript:alert('かみんぐすぅん');"><img src="../img/nav_death.png" alt="死にかけリスト"></a></li>
+			<li><a href="javascript:alert('かみんぐすぅん');"><img src="../img/nav_grave.png" alt="墓地"></a></li>
 		</ul>
-	</div>
-
-
-	<div class="meigen">
-		<div class="meigen-area">
-			<div class="meigen-photo"><img src="<?php echo $meigen['image_url']; ?>"></div>
-			<div class="meigen-txt">
-				<div class="meigen-detail">
-				<h2>高橋くんデザインお願いします</h2>
-				<?php
-					echo '<p>' . nl2br(htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['contributor'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['speaker'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['meigen_text'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['unit_name'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['situation'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['font'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['member_id'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['image_url'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['created_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($meigen['modified_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-				?>
+	</nav>
+	<div id="meigen_box">
+		<div class="meigen" id="meigen_list<?php echo $meigen['meigen_id']; ?>">
+			<div class="meigen-area cf">
+				<a href="./detail.php?meigen_id=<?php print $meigen['meigen_id']; ?>">
+				<div class="meigen-photo"><img src="../images/member/<?php echo $meigen['member_id']; ?>.jpg"></div>
+				<div class="meigen-txt">
+                                    <h2 style="font-family: <?php echo $arrFont["{$meigen['font']}"];?>"><?php echo nl2br($meigen['situation']); ?></h2>
+					<div class="meigen-detail">
+					<p class="date"><?php echo $meigen['created_at']; ?></p>
+					<p class="name"><?php echo $meigen['speaker']; ?></p>
+					</div>
 				</div>
-				<div class="meigen-detail">
-				<?php
-				foreach ($tries as $try) {
-					echo '<p>' . nl2br(htmlspecialchars($try['contributor'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['meigen_id'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['unit_name'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['font'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['member_id'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['image_url'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['created_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-					echo '<p>' . nl2br(htmlspecialchars($try['modified_at'], ENT_QUOTES,'UTF-8')) . '</p>';
-				}
-				?>
+				</a>
+			</div>
+			<div class="tamashii-area cf">
+				<div class="tamashii" id="haa_count<?php echo $meigen['meigen_id']; ?>">
+                                </div>
+			</div>
+		</div>
+            <?php
+            foreach ($tries as $try) {
+            ?>
+		<div class="meigen">
+			<div class="meigen-area cf">
+				<div class="meigen-photo"><img src="../images/member/<?php echo $try['member_id']; ?>.jpg"></div>
+				<div class="meigen-txt">
+                                    <h2 style="font-family: <?php echo $arrFont["{$try['font']}"];?>">まだコメントは投稿できません</h2>
 				</div>
 			</div>
 		</div>
-		<div class="ha-area">
-			<ul>
-				<li><a href="../meigen/index.php">名言</a></li>
-				<li><a href="../meigen/try.php?meigen_id=<?php echo htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>">使ってみた登録</a></li>
-			</ul>
-		</div>
-	</div>
+            <?php
+            }
+            ?>
 
+        </div>
 </div>
-<a href="../meigen/">めいげん</a>
 </body>
 </html>
--- meigen/try.php
+++ meigen/try.php
@@ -3,11 +3,15 @@ require_once('../libs/core.php');
 
 session_start();
 
-$params = array_merge($_POST, $_GET);
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $params = $_POST;    
+} else {
+    $params = $_GET;
+}
 
 $objDb = new db_util();
 
-$meigen = array_pop($objDb->select('select * from meigens where meigen_id = ?', [ $params['meigen_id'] ]));
+$meigen = array_pop($objDb->select('select * from meigens where meigen_id = ?',  array($params['meigen_id'])));
 if (is_null($meigen)) {
 	header("HTTP/1.0 404 Not Found");
 	die();
@@ -15,7 +19,7 @@ if (is_null($meigen)) {
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 	$member_id = $params['member_id'];
-	$member = array_pop($objDb->select('select * from members where member_id = ?', [ $member_id ]));
+	$member = array_pop($objDb->select('select * from members where member_id = ?', array($member_id)));
 	if (is_null($member)) {
 		header("HTTP/1.0 404 Not Found");
 		die();
@@ -25,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 	// $contributor = $_SESSION['facebook_user_id'];
 	$contributor = 'dummy-contributorr';
 
-	$unit = array_pop($objDb->select('select * from units where unit_id = ?', [ $member['unit_id'] ]));
+	$unit = array_pop($objDb->select('select * from units where unit_id = ?', array($member['unit_id'])));
 	if (is_null($unit)) {
 		header("HTTP/1.0 404 Not Found");
 		die();
@@ -53,14 +57,24 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 }
 
 $members = $objDb->select("SELECT * FROM members;");
-?><!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<title>雰囲気わかりました</title>
-	</head>
-	<body>
-		<form name="form_regist" action="./try.php" method="POST">
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>名言くん（仮） ver.amagasaki 新規登録</title>
+<meta name="description" content="">
+<link type="text/css" rel="stylesheet" href="../css/main.css" />
+</head>
+<body>
+<div class="container">
+	<header class="cf">
+            <div class="navbar-brand"> <a href="./"><img src="../img/title.png"></a></div>
+		<div class="navbar-entry"><a href="./nyuu.php"><img src="../img/bt_entry.png" alt="投稿する"></a></div>
+	</header>
+            <form name="form_regist" action="./try.php" method="POST">
 			<input type="hidden" name="meigen_id" value="<?php print htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>" id="meigen_id" />
 			<p>発言者(必須): 
 				<select name="member_id" id="member_id">
@@ -83,8 +97,7 @@ $members = $objDb->select("SELECT * FROM members;");
 
 			<input type="submit" value="投稿する" />
 		</form>
-
-		<br>
-		<a href="../meigen/detail.php?meigen_id=<?php print htmlspecialchars($meigen['meigen_id'], ENT_QUOTES, 'UTF-8'); ?>">名言</a>
-	</body>
+        </div>
+    </div>
+</body>
 </html>
