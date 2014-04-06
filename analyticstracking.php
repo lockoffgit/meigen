fab9b097835acec07167bd317d676b2dff8407ff
kuehara <kuehara@192.168.11.8>
2014-04-06 23:44:26 +0900

    googleanalytics コード追加

--- analyticstracking.php
+++ analyticstracking.php
@@ -1,10 +1,5 @@
 <?php
 $return .= <<<EOF
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
