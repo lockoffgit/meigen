ea668fa5534e1dcb6c1373e9c7d03f9723343bc9
tattin <takahashi.capsor@gmail.com>
2014-04-15 22:14:42 +0900

    APIに画像を追加

--- meigen/api.php
+++ meigen/api.php
@@ -10,12 +10,20 @@
 
             $objDb = new db_util();
             $arrMeigens = $objDb->getMeigenList($page_no, $sort_key);
+            $arrMeigens = imageInsert($arrMeigens);
             echo json_encode($arrMeigens);
         break;
 
         case 'one' :
             $arrMeigen = $objDb->getMeigenData($_REQUEST['meigen_id']);
+            $arrMeigen['meigen_image_url'] = 'http://news.utamap.com/_upimg/04400/aa4400_02_0622233106.jpg';
             echo json_decode($arrMeigen);
         break;
+    }
+    function imageInsert($array){
+        foreach($array as $k => $v){
+            $array[$k]['meigen_image_url'] = 'http://news.utamap.com/_upimg/04400/aa4400_02_0622233106.jpg';
+        }
+        return $array;
     }
 ?>
